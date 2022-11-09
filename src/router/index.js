71f13301657f0b0c0513129448ea7f54e13ccdb6
dingjiabao <1366928952@qq.com>
2022-11-09 10:27:58 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: dingjiabao
+ * @Date: 2022-11-08 17:09:34
+ * @LastEditors: dingjiabao
+ * @LastEditTime: 2022-11-08 17:44:27
+ * @Description:
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
